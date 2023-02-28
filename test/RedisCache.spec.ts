b2ac69a975064f8ef6aba0f7b292af7eec0cf13a
Igor Savin <iselwin@gmail.com>
2023-02-28 14:42:43 +0200

    Use SCAN instead of KEYS (#199)

--- test/RedisCache.spec.ts
+++ test/RedisCache.spec.ts
@@ -46,6 +46,36 @@ describe('RedisCache', () => {
       expect(value2).toBeUndefined()
     })
 
+    it('clears empty storage', async () => {
+      const cache = new RedisCache(redis)
+      await cache.clear()
+
+      const value1 = await cache.get('key')
+
+      expect(value1).toBeUndefined()
+    })
+
+    it('clears chunked values', async () => {
+      const cache = new RedisCache(redis)
+      for (let x = 0; x < 1500; x++) {
+        await cache.set(`key${x.toString()}`, 'value')
+      }
+      const key = 'key5'
+      const key2 = 'key1005'
+      const value1Pre = await cache.get(key)
+      const value2Pre = await cache.get(key2)
+      expect(value1Pre).toBe('value')
+      expect(value2Pre).toBe('value')
+
+      await cache.clear()
+
+      const value1 = await cache.get(key)
+      const value2 = await cache.get(key2)
+
+      expect(value1).toBeUndefined()
+      expect(value2).toBeUndefined()
+    })
+
     it('does not clear values from other caches', async () => {
       const cache = new RedisCache(redis, { prefix: 'c1' })
       const cache2 = new RedisCache(redis, { prefix: 'c2' })
@@ -85,6 +115,15 @@ describe('RedisCache', () => {
   })
 
   describe('deleteGroup', () => {
+    it('clears empty group', async () => {
+      const cache = new RedisCache(redis)
+
+      await cache.deleteGroup('group2')
+
+      const value = await cache.getFromGroup('dummy', 'group2')
+      expect(value).toBeUndefined()
+    })
+
     it('deletes values matching the group pattern', async () => {
       const cache = new RedisCache(redis)
       await cache.setForGroup('key', 'value', 'team1')
@@ -103,6 +142,37 @@ describe('RedisCache', () => {
       expect(value2t1).toBe('value2')
       expect(value1t2).toBeUndefined()
       expect(value2t2).toBeUndefined()
+    })
+
+    it('clears chunked values', async () => {
+      const cache = new RedisCache(redis)
+      const group1 = 'group1'
+      const group2 = 'group2'
+      for (let x = 0; x < 1500; x++) {
+        await cache.setForGroup(`key${x.toString()}`, 'value', group1)
+        await cache.setForGroup(`key${x.toString()}`, 'value2', group2)
+      }
+      const key = 'key5'
+      const key2 = 'key1005'
+      const value1PreGroup1 = await cache.getFromGroup(key, group1)
+      const value2PreGroup1 = await cache.getFromGroup(key2, group1)
+      const value1PreGroup2 = await cache.getFromGroup(key, group2)
+      const value2PreGroup2 = await cache.getFromGroup(key2, group2)
+      expect(value1PreGroup1).toBe('value')
+      expect(value2PreGroup1).toBe('value')
+      expect(value1PreGroup2).toBe('value2')
+      expect(value2PreGroup2).toBe('value2')
+
+      await cache.deleteGroup(group2)
+
+      const value1Group1 = await cache.getFromGroup(key, group1)
+      const value2Group1 = await cache.getFromGroup(key2, group1)
+      const value1Group2 = await cache.getFromGroup(key, group2)
+      const value2Group2 = await cache.getFromGroup(key2, group2)
+      expect(value1Group1).toBe('value')
+      expect(value2Group1).toBe('value')
+      expect(value1Group2).toBeUndefined()
+      expect(value2Group2).toBeUndefined()
     })
   })
 
